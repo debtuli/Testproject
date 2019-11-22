77fc778c9f206eaedaa05ff0d45df245f76ef9b4
debtuli <debtuli@gmail.com>
2019-11-22 15:51:43 +0530

    Updated code

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
-  echo "<body style='background-color:pink'>";
+  echo "<body style='background-color:blue'>";
  <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
